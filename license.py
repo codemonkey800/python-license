9cc9192999188de75384c247aae961d1d3d4b196
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-12-01 04:45:09 -0800

    Setup setup.py script and added license executable

---
+++ license.py
@@ -0,0 +1,6 @@
+def main():
+    print('lmao what')
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
 
 setup(
-
+    name='license',
+    version='1.0',
+    description=('A command line app that fetches open '
+                 'source licenses from https://opensource.org.'),
+    author='Jeremy Asuncion',
+    author_email='jeremyasuncion808@gmail.com',
+    url='https://gitlab.com/codemonkey800/python-license',
+    license='MIT',
+    py_modules=['license'],
+    scripts=['license'],
 )
-
